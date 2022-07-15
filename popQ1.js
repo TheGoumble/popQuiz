f0a08a3427f29ecd238de38133f3ef7e4c7593bd
Javier Vargas <thebruh@Javiers-MacBook-Air.local>
2022-07-15 10:23:00 -0400

    popQuiz 1

---
+++ popQ1.js
@@ -0,0 +1,33 @@
+//My black sheet of paper
+//Create an object with your name, age and indicate whether you are lost or not.
+
+let obj = {
+  name: "jeff",
+  age: "10",
+  isLost: false,
+}
+randoArray = [
+  { name: "Matt", age: 19 },
+  { name: "Todd", age: 46 },
+]
+person = { name: "Todd", age: 46, awesome: true }
+
+//person['name'] = 'Albert'
+//person.name = 'Albert'
+console.log(person)
+
+//Write a function that adds two numbers (return the result): // paste from your code editor
+function sum(x, y) {
+  return x + y
+}
+
+let a = [1, 2, 3, 4]
+let x = a.pop()
+
+console.log(x)
+
+function counter(){
+    for(let i = 0; i < 11; i++){
+        console.log(i)
+    }
+}
